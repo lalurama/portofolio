a157b11e964c72dcbcdc27ecd558e49d0b435b57
lalurama <lalurama26@gmail.com>
2023-01-23 13:33:45 +0800

    belum slse

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,26 @@
-function swipe() {
-    const button = document.getElementById("click");
+
+
+    var button = document.getElementById("click");
+    var target = document.getElementById("wrapper");
+    var button2 = document.getElementById("tutup");    
     
-}
+    button.addEventListener("click", alert("success"));
+
+    button.addEventListener("click", function (){
+        alert ("success")
+        target.style.height = "250px";
+        button.innerHTML = "tutup";
+        if (target.style.height === "250px") {
+            button.id = "tutup"
+        }
+    });
+    
+
+    button.addEventListener("click", function(){
+        target.style.height = "50px";
+        button2.innerHTML = "read";
+        if (target.style.height === "50px") {
+            button2.id = "read"
+        }
+    });
+    
