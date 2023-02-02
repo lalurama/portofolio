0dcdebbfa52940c6d321459ce232957b66f20a65
lalurama <lalurama26@gmail.com>
2023-02-02 11:06:57 +0800

    about belum jadi

--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,23 @@
 
-
-    // var button = document.getElementById("click");
-    // var target = document.getElementById("wrapper");
-    // var button2 = document.getElementById("tutup");    
+window.addEventListener('scroll', function () {
+    var navbar = document.querySelector("nav");
+    var range = window.pageYOffset;
+    var serviceR = document.getElementById('serviceR');
+    var serviceL = document.getElementById('serviceL');
+    console.log(range)
+    if (range >= 100) {
+        navbar.style.position = 'fixed';
+        navbar.style.top = 0;
+    } else {
+        navbar.style.position = 'relative';
+        navbar.style.top = 20 + 'px'
+    }
     
-    // button.addEventListener("click", alert("success"));
-
-    // button.addEventListener("click", function (){
-    //     alert ("success")
-    //     target.style.height = "250px";
-    //     button.innerHTML = "tutup";
-    //     if (target.style.height === "250px") {
-    //         button.id = "tutup"
-    //     }
-    // });
-    
-
-    // button.addEventListener("click", function(){
-    //     target.style.height = "50px";
-    //     button2.innerHTML = "read";
-    //     if (target.style.height === "50px") {
-    //         button2.id = "read"
-    //     }
-    // });
-    window.addEventListener('scroll', function () {
-        var navbar = document.querySelector("nav");
-        var range = window.scrollY;
-        console.log(range);
-        if (window.pageYOffset >= range) {
-            navbar.style.position = "fixed";
-            navbar.style.top = 0;
-        } else {
-            navbar.style.position = "relative";
-        }
-    })
-
-    
-
+    if (range >= 900) {
+        serviceL.style.marginRight = 10+'px';
+        serviceR.style.marginLeft = 10+'px';
+    } else {
+        serviceL.style.marginRight= 400+'px';
+        serviceR.style.marginLeft = 400+'px';
+    }
+})
